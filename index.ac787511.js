febf9f0320405ffc634dbf59f982b47abae9db76
Vlad Popenko <krokodil94dynamo@gmail.com>
2024-11-05 15:13:03 +0200

    make build

---
+++ index.ac787511.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var t=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(e){e.preventDefault();var n=e.target.closest("a");if(n){var r=n.getAttribute("href");t.setAttribute("src",r)}})});
+//# sourceMappingURL=index.ac787511.js.map
